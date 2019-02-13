c7058f2cd505d7e37502dd6b1bbb70c29df5a807
4m1g0 <dev.4m1g0@gmail.com>
2019-02-13 20:16:45 +0100

    Changed gitignore and removed files uploaded by mistake

--- hardware/fingerprintSensor/src/config/config.h
+++
@@ -1,8 +0,0 @@
-
-const char* WIFI_SSID = "bricolabsiot";
-const char* WIFI_PASSWORD = "4D&jfhJtnt8g";
-
-const char* MQTT_USER = "iot";
-const char* MQTT_PASSWORD = "mosquittoIotPsw";
-
-const char* MQTT_ADDR = "10.3.141.1";
